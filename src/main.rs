0f1c0612dda8200bfd3a66cdc50cb1da6422ff2f
Zeke Foppa <benjamin.foppa@gmail.com>
2015-12-26 10:38:51 -0500

    Play back a two-second buffer

--- src/main.rs
+++ src/main.rs
@@ -28,25 +28,28 @@ fn errorful_main() -> Result<(), String> {
   let f = 44100;
   let buf_size = 1 << 10;
 
-//  info!("Reading..");
-//
-//  let mut stream: portaudio::pa::Stream<f32, f32> = portaudio::pa::Stream::new();
-//  try!(string_err(
-//    stream.open_default_blocking(
-//      44100.0,
-//      64,
-//      2,
-//      0,
-//      portaudio::pa::SampleFormat::Float32,
-//    )));
-//  try!(string_err(stream.start()));
-//  let mut buf = Vec::new();
-//  while buf.len() < 1000000 {
-//    let mut new = try!(string_err(stream.read(64)));
-//    buf.append(&mut new);
-//  }
-//
-//  try!(string_err(stream.stop()));
+  info!("Reading..");
+
+  let mut stream: portaudio::pa::Stream<f32, f32> = portaudio::pa::Stream::new();
+  try!(string_err(
+    stream.open_default_blocking(
+      f as f64,
+      buf_size,
+      1,
+      0,
+      portaudio::pa::SampleFormat::Float32,
+    )));
+  try!(string_err(stream.start()));
+
+  let mut buf = std::collections::vec_deque::VecDeque::new();
+  let start_time = time::precise_time_ns();
+  while time::precise_time_ns() <= start_time + 2_000_000_000 {
+    let new = try!(string_err(stream.read(buf_size)));
+    assert!(new.len () == buf_size as usize);
+    buf.push_back(new);
+  }
+
+  try!(string_err(stream.stop()));
 
   info!("Writing..");
 
@@ -61,14 +64,9 @@ fn errorful_main() -> Result<(), String> {
     )));
   try!(string_err(stream.start()));
 
-  let start_time = time::precise_time_ns();
-  let mut i = 0;
-  while time::precise_time_ns() <= start_time + 2_000_000_000 {
-    let buf = sine_wav(f as f32, i*buf_size, (i+1)*buf_size);
+  while let Some(buf) = buf.pop_front() {
     assert!(buf.len() == buf_size as usize);
     try!(string_err(stream.write(buf, buf_size)));
-
-    i = i + 1;
   }
 
   try!(string_err(stream.stop()));
