5c936218de3dc6e36d8946727df77e7105659769
Zeke Foppa <benjamin.foppa@gmail.com>
2015-12-26 15:08:48 -0500

    Print human-readable notes

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn scale_step() -> f64 {
   (2.0 as f64).powf(1.0 / 12.0)
 }
 
-fn scale_steps(f: f64) -> u32 {
+fn scale_steps(f: f64) -> i32 {
   let ratio = f / 440.0;
   let steps = ratio.log(scale_step());
-  steps.round() as u32
+  steps.round() as i32
 }
 
 fn string_err<Ok, Err: std::fmt::Display>(r: Result<Ok, Err>) -> Result<Ok, String> {
@@ -67,8 +67,25 @@ fn play_pitch() -> Result<(), String> {
   Ok(())
 }
 
-fn detect_pitch() -> Result<(), String> {
-  let f = 44100;
+fn display_note(note: u32) -> &'static str {
+  let notes = [
+    "A",
+    "A#/Bb",
+    "B",
+    "C",
+    "C#/Db",
+    "D",
+    "D#/Eb",
+    "E",
+    "F",
+    "F#/Gb",
+    "G",
+    "G#/Ab",
+  ];
+  notes[note as usize]
+}
+
+fn record(sample_frequency: f64) -> Result<Vec<f64>, String> {
   let buf_size = 1 << 10;
   assert!(buf_size & (buf_size - 1) == 0);
 
@@ -77,7 +94,7 @@ fn detect_pitch() -> Result<(), String> {
   let mut stream: portaudio::pa::Stream<f32, f32> = portaudio::pa::Stream::new();
   try!(string_err(
     stream.open_default_blocking(
-      f as f64,
+      sample_frequency,
       buf_size,
       1,
       0,
@@ -95,18 +112,18 @@ fn detect_pitch() -> Result<(), String> {
 
   try!(string_err(stream.stop()));
 
-  info!("Collected {} samples", buf.len());
+  Ok(buf)
+}
 
-  info!("Pitch detecting..");
-
-  let len = buf.len();
-  let r = rgsl::fft::real_radix2::transform(&mut buf, 1, len);
+fn detect_frequency(mut samples: Vec<f64>, timestep: f64) -> Result<f64, String> {
+  let len = samples.len();
+  let r = rgsl::fft::real_radix2::transform(&mut samples, 1, len);
   if r != rgsl::Value::Success {
     return Err(format!("rgsl returned {:?}", r));
   }
 
   let (max_idx, _max_val) =
-    buf.iter()
+    samples.iter()
       .enumerate()
       .take(len / 2)
       .fold((0, 0.0), |(max_idx, max_val), (i, val)| {
@@ -117,10 +134,48 @@ fn detect_pitch() -> Result<(), String> {
         }
       });
 
-  info!("Max index is {}", max_idx);
-  let f = max_idx as f64 / buf.len() as f64 * f as f64;
-  println!("Estimated frequency is {}", f);
-  println!("{} semitones away from middle A", scale_steps(f));
+  debug!("Max index is {}", max_idx);
+  Ok(max_idx as f64 / samples.len() as f64 / timestep)
+}
+
+fn human_readable_frequency(f: f64) -> String {
+  let scale_steps = scale_steps(f);
+
+  info!("Estimated frequency is {} ({} semitones above middle A)", f, scale_steps);
+
+  let octave = {
+    // Octave is decided by the Cs, so make scale_steps relative to C4.
+    let scale_steps = scale_steps + 9;
+    4 + scale_steps / 12
+  };
+  let note = {
+    let note = scale_steps % 12;
+    let note = if note >= 0 { note } else { note + 12 };
+    assert!(note >= 0);
+    assert!(note < 12);
+    note as u32
+  };
+
+  let note = display_note(note);
+
+  if octave == 4 {
+    format!("middle {} ({}{})", note, note, octave)
+  } else {
+    format!("{}{}", note, octave)
+  }
+}
+
+fn detect_pitch() -> Result<(), String> {
+  let sample_frequency = 44100.0;
+  let samples = try!(record(sample_frequency));
+
+  info!("Collected {} samples", samples.len());
+
+  info!("Detecting pitch..");
+
+  let f = try!(detect_frequency(samples, 1.0 / sample_frequency));
+
+  println!("Estimated note is {}", human_readable_frequency(f));
 
   Ok(())
 }
